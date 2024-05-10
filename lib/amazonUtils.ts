1af571caf67cfd6792fce2efc08ad319164d0bfd
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-05-10 21:50:25 +0530

    feat: Implement utility functions for scraping and processing price data

---
+++ lib/amazonUtils.ts
@@ -0,0 +1,71 @@
+type PriceHistoryItem = {
+  price: number;
+};
+
+export function extractDescription($: any) {
+  const elements = $('#feature-bullets .a-unordered-list .a-spacing-mini span');
+  if (elements.length > 0) {
+    const textContent = elements
+      .map((_: any, element: any) => $(element).text().trim())
+      .get()
+      .join('/n');
+    return textContent;
+  }
+  return '';
+}
+
+export function extractDiscount(discount: any) {
+  const element = discount.text().trim().replace(/[-%]/g, '');
+  return element;
+}
+
+export function extractPrice(...elements: any) {
+  for (const element of elements) {
+    const priceText = element.text().trim();
+
+    if (priceText) {
+      const cleanPrice = priceText.replace(/[^\d.]/g, '');
+
+      let firstPrice;
+
+      if (cleanPrice) {
+        firstPrice = cleanPrice.match(/\d+\.\d{2}/)?.[0];
+      }
+
+      return firstPrice || cleanPrice;
+    }
+  }
+
+  return '';
+}
+
+export function getHighestPrice(priceList: PriceHistoryItem[]) {
+  let highestPrice = priceList[0];
+
+  for (let i = 0; i < priceList.length; i++) {
+    if (priceList[i].price > highestPrice.price) {
+      highestPrice = priceList[i];
+    }
+  }
+
+  return highestPrice.price;
+}
+
+export function getLowestPrice(priceList: PriceHistoryItem[]) {
+  let lowestPrice = priceList[0];
+
+  for (let i = 0; i < priceList.length; i++) {
+    if (priceList[i].price < lowestPrice.price) {
+      lowestPrice = priceList[i];
+    }
+  }
+
+  return lowestPrice.price;
+}
+
+export function getAveragePrice(priceList: PriceHistoryItem[]) {
+  const sumOfPrices = priceList.reduce((acc, curr) => acc + curr.price, 0);
+  const averagePrice = sumOfPrices / priceList.length || 0;
+
+  return averagePrice;
+}
