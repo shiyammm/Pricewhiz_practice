879de810805816ef079989f5566b64ae65de88a7
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-05-13 22:48:30 +0530

    Import the getProductsById function to fetch individual product details and populated the title, price, image, category, and other relevant details about the product

--- app/search/amazon/product/[productId]/page.tsx
+++ app/search/amazon/product/[productId]/page.tsx
@@ -1,7 +1,75 @@
-import React from 'react';
+import { getProductById } from '@/lib/actions/amazon';
+import { redirect } from 'next/navigation';
 
-const page = ({ params }: { params: { productId: string } }) => {
-  return <div>Product No: {params.productId}</div>;
+type Props = {
+  params: { productId: string };
+};
+
+const page = async ({ params: { productId } }: Props) => {
+  const product = await getProductById(productId);
+
+  if (!product) redirect('/');
+
+  return (
+    <div>
+      <div>
+        <h1>
+          <span>{product.title}</span>
+        </h1>
+        <img src={product.image} alt={product.title} />
+        <p>
+          <span>
+            <strong>Price:</strong> {product.currency}
+            {product.currentPrice}
+          </span>
+          <br />
+          <span>
+            <strong>Original Price:</strong> {product.currency}
+            {product.originalPrice}
+          </span>
+          <br />
+          <span>
+            <strong>Discount Rate:</strong> {product.discountRate}%
+          </span>
+        </p>
+        <p>
+          <span>
+            <strong>Lowest Price:</strong> {product.currency}
+            {product.lowestPrice}
+          </span>
+          <br />
+          <span>
+            <strong>Highest Price:</strong> {product.currency}
+            {product.highestPrice}
+          </span>
+          <br />
+          <span>
+            <strong>Average Price:</strong> {product.currency}
+            {product.averagePrice}
+          </span>
+        </p>
+        <p>
+          <span>
+            <strong>Category:</strong> {product.category}
+          </span>
+          <br />
+          <span>
+            <strong>Reviews Count:</strong> {product.reviewsCount}
+          </span>
+          <br />
+          <span>
+            <strong>Out of Stock:</strong> {product.isOutOfStock ? 'Yes' : 'No'}
+          </span>
+        </p>
+        <p>
+          <span>{product.description}</span>
+        </p>
+      </div>{' '}
+      <br />
+      <br />
+      <br />
+    </div>
+  );
 };
 
 export default page;
